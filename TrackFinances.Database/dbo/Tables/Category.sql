059a9111991e5c32edbd90d88e9773d1be50f433
Aaron Yota <aiyota@outlook.com>
2022-04-17 12:41:15 -0700

    Add unique constraint on Category Name column.

--- TrackFinances.Database/dbo/Tables/Category.sql
+++ TrackFinances.Database/dbo/Tables/Category.sql
@@ -4,5 +4,6 @@
     [Name] NVARCHAR(100) NOT NULL, 
     [Description] NVARCHAR(1000) NULL,
     [DateCreated] DATETIME2 NOT NULL DEFAULT GETDATE(), 
-    [DateModified] DATETIME2 NULL
+    [DateModified] DATETIME2 NULL, 
+    CONSTRAINT [UQ_Category_Name] UNIQUE ([Name])
 )
