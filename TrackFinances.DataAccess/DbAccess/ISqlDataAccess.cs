a2d7ec325d08ffca1a34f954bb424f67e4bca7dc
Aaron Yota <aiyota@outlook.com>
2022-04-16 20:07:33 -0700

    Create SqlDataAccess class/interface.

---
+++ TrackFinances.DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TrackFinances.DataAccess.DbAccess;
+
+public interface ISqlDataAccess
+{
+    Task<bool> ExecuteAsync<T>(string storedProcedure,
+                               T parameters,
+                               string connectionId = "Default");
+    Task<IEnumerable<T>> QueryAsync<T, U>(string storedProcedure,
+                                          U parameters,
+                                          string connectionId = "Default");
+    Task<T> QueryFirstOrDefaultAsync<T, U>(string storedProcedure,
+                                           U parameters,
+                                           string connectionId = "Default");
+}
---
+++ TrackFinances.DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackFinances.DataAccess.DbAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<IEnumerable<T>> QueryAsync<T, U>(string storedProcedure,
+                                                       U parameters,
+                                                       string connectionId = "Default")
+    {
+        using var connection = await CreateConnectionAsync(connectionId);
+        return await connection.QueryAsync<T>(storedProcedure,
+                                              parameters,
+                                              commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<T> QueryFirstOrDefaultAsync<T, U>(string storedProcedure,
+                                                        U parameters,
+                                                        string connectionId = "Default")
+    {
+        using var connection = await CreateConnectionAsync(connectionId);
+        return await connection.QueryFirstOrDefaultAsync<T>(storedProcedure,
+                                                            parameters,
+                                                            commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<bool> ExecuteAsync<T>(string storedProcedure,
+                                            T parameters,
+                                            string connectionId = "Default")
+    {
+        using var connection = await CreateConnectionAsync(connectionId);
+        var rowsAffected = await connection.ExecuteAsync(storedProcedure,
+                                                         parameters,
+                                                         commandType: CommandType.StoredProcedure);
+        return rowsAffected > 0;
+    }
+
+    private async Task<IDbConnection> CreateConnectionAsync(string connectionId)
+    {
+        var connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+        await connection.OpenAsync();
+        return connection;
+    }
+}
