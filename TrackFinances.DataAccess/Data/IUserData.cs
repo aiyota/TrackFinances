f7253d2b9151add179c0b8fa2fcc3e8f18b38b77
Aaron Yota <aiyota@outlook.com>
2022-04-17 08:58:16 -0700

    Create User data access.

---
+++ TrackFinances.DataAccess/Data/IUserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackFinances.DataAccess.Models;
+
+namespace TrackFinances.DataAccess.Data;
+
+public interface IUserData
+{
+    Task<User> GetAsync(string id);
+    Task<User> GetByEmailAsync(string email);
+    Task<User> GetByUserName(string username);
+    Task<bool> UpdateAsync(User user);
+    Task<User> CreateAsync(User user);
+    Task<bool> DeleteAsync(string id);
+}
---
+++ TrackFinances.DataAccess/Data/UserData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackFinances.DataAccess.DbAccess;
+using TrackFinances.DataAccess.Models;
+
+namespace TrackFinances.DataAccess.Data;
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _database;
+
+    public UserData(ISqlDataAccess database)
+    {
+        _database = database;
+    }
+
+    public async Task<User> GetAsync(string id)
+    {
+        return await _database.QueryFirstOrDefaultAsync<User, dynamic>("spUser_Get", new { Id = id });
+    }
+
+    public async Task<User> GetByEmailAsync(string email)
+    {
+        return await _database.QueryFirstOrDefaultAsync<User, dynamic>("spUser_GetByEmail", new { Email = email });
+    }
+
+    public async Task<User> GetByUserName(string username)
+    {
+        return await _database.QueryFirstOrDefaultAsync<User, dynamic>("spUser_GetByUserName", new { UserName = username });
+    }
+
+    public async Task<bool> UpdateAsync(User user)
+    {
+        return await _database.ExecuteAsync<dynamic>("spUser_Update", user);
+    }
+
+    public async Task<User> CreateAsync(User user)
+    {
+        return await _database
+                        .QueryFirstOrDefaultAsync<User, User>("spUser_Create", user);
+    }
+
+    public async Task<bool> DeleteAsync(string id)
+    {
+        return await _database.ExecuteAsync<dynamic>("spExpense_Delete", new { Id = id });
+    }
+}
