655d2f1d50f02908f34c3e4400eb1d7e83d09062
Aaron Yota <aiyota@outlook.com>
2022-04-16 21:24:18 -0700

    Create database models.

---
+++ TrackFinances.DataAccess/Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackFinances.DataAccess.Models;
+
+public class Expense
+{
+    public int Id { get; set; } = default!;
+    public Guid UserId { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string? Description { get; set; }
+    public decimal Charge { get; set; } = default!;
+    public DateTime? ChargeDate { get; set; }
+    public int CategoryId { get; set; } = default!;
+    public bool IsDirectCharge { get; set; } = default!;
+    public bool IsCleared { get; set; } = default!;
+    public DateTime? DateCreated { get; set; }
+}
---
+++ TrackFinances.DataAccess/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackFinances.DataAccess.Models;
+public class User
+{
+    public Guid Id { get; set; } = default!;
+    public string UserName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string PasswordHash { get; set; } = default!;
+    public bool EmailConfirmed { get; set; } = default!;
+    public DateTime? DateJoined { get; set; } 
+}
